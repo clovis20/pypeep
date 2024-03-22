8346d95a629342923a1bf21a810bf1a9bc6f3587
clovis20 <acetruco@hotmail.com>
2024-03-22 15:56:15 -0300

    add navbar and profile page

--- pypeepapp/models.py
+++ pypeepapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create a User Profile Model
 class Profile(models.Model):
@@ -9,8 +10,18 @@ class Profile(models.Model):
                 symmetrical=False,
                 blank=True)
     
+    date_modified = models.DateTimeField(User, auto_now=True)
+    
     def __str__(self):
         return self.user.username
 
 
-# Create your models here.
+# Create Profile when new User Signs Up
+def create_profile(sender, instance, created,  **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+
+post_save.connect(create_profile, sender=User)
