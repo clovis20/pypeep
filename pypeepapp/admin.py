2bae6152a04d5389b7c9608f68626b27fc473b4f
clovis20 <acetruco@hotmail.com>
2024-03-22 09:05:05 -0300

    adding User + Profiles

--- pypeepapp/admin.py
+++ pypeepapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister Groups
+admin.site.unregister(Group)
+
+# Mix Profile Info into User Info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just display username fields on admin page
+    fields = ['username']
+    inlines = [ProfileInline]
+
+# Unregister initial User
+admin.site.unregister(User)
+
+# Register User and Profile
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
